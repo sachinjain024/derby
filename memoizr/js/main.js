d7a98e8aef31598765e29d91a2450f4b3e88d480
Sachin <sachin@jain024gmail.com>
2013-05-21 23:17:11 +0530

    UI  updates as per new mocks. low level working only

--- memoizr/js/main.js
+++ memoizr/js/main.js
@@ -2,10 +2,11 @@ var Mz = Mz || {};
 
 // Defining Difficulty level 
 Mz.LEVELS = {
-	LOW		: { value: 0, time: 6000, numPics: 5 },
-	MEDIUM	: { value: 1, time: 4000, numPics: 5 },
-	HIGH	: { value: 2, time: 2000, numPics: 5 }
+	LOW		: { value: 0, time: 3000, numPics: 6 },
+	MEDIUM	: { value: 1, time: 3000, numPics: 8 },
+	HIGH	: { value: 2, time: 3000, numPics: 10 }
 };
+
 Mz.filters=[
               ["flipXY",{}],
               ["bulge",{}],
@@ -14,6 +15,7 @@ Mz.filters=[
               ["randomjitter",{}],
               ["pixelate",{pixelSize:5}]
           ];
+
 // By Default, Low level is selected
 Mz.selectedLevel = Mz.LEVELS.LOW;
 
@@ -26,9 +28,12 @@ Mz.numSnapshots = 0;
 
 // Define all event handlers here
 $('.start-game').on('click', playVideo);
+$('.nav-option').on('click', changeGameLevel);
 
 var video = null;
 function playVideo() {
+  
+  refreshObjects();
 	try {
 		window.navigator.mozGetUserMedia(Mz.gumVideoPermission, function(stream) {
 			// Remove video object if already exists
@@ -45,36 +50,59 @@ function playVideo() {
 	}
 }
 
-var inputContainer = $('#input-row');
 function startTakingPictures() {
-	if (Mz.numSnapshots > Mz.selectedLevel.numPics) return;
 	// Break recursion if number of snapshots are complete as per selected level
-
+  if (Mz.numSnapshots > Mz.selectedLevel.numPics) return;
+	
 	var canvas = document.createElement('canvas');
 	var ctx = canvas.getContext('2d');
 
-  	canvas.width  = video.videoWidth/4;
-  	canvas.height = video.videoHeight/4;
-  	ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-  	  	
-  	canvas.setAttribute('data-index', rand);
-        var tData = ctx.createImageData(canvas.width-1,canvas.height-1);
-        var filterIdx = ~~(Math.random()*Mz.filters.length);
-        $filterjs.getFilter(Mz.filters[filterIdx][0])(ctx.getImageData(0,0,canvas.width-1,canvas.height-1),tData,Mz.filters[filterIdx][1]);
+	canvas.width  = video.videoWidth/4;
+	canvas.height = video.videoHeight/4;
+	ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+	  	
+  // Applying random filter on canvas
+  var tData = ctx.createImageData(canvas.width-1,canvas.height-1);
+  var filterIdx = ~~(Math.random()*Mz.filters.length);
+  $filterjs.getFilter(Mz.filters[filterIdx][0])(ctx.getImageData(0,0,canvas.width-1,canvas.height-1),tData,Mz.filters[filterIdx][1]);
 
-        var rand = getRandom();
-        Mz.snapshotTimestamps.push(rand);
-        Mz.numSnapshots++;
-        ctx.putImageData(tData,0,0);
-  	$('<li>').addClass('thumbnail').append(canvas).appendTo(inputContainer);
+  var rand = getRandom();
+  Mz.snapshotTimestamps.push(rand);
+  ctx.putImageData(tData,0,0);
 
-  	if (Mz.numSnapshots <= Mz.selectedLevel.numPics) {
-  		setTimeout(startTakingPictures, Mz.selectedLevel.time);
-  		return;
-  	}
-  	video.pause();
+  console.log(tData);
+  console.log(Mz.numSnapshots);
+
+  var image = $('<img />').attr({'src': canvas.toDataURL(), 'data-index': rand}); 
+	$('#item-' + Mz.numSnapshots).empty().append(image);
+
+  Mz.numSnapshots++;
+	if (Mz.numSnapshots <= Mz.selectedLevel.numPics) {
+		setTimeout(startTakingPictures, Mz.selectedLevel.time);
+		return;
+	}
+	video.pause();
 }
 
 function getRandom() {
 	return new Date().getTime();
+}
+
+function changeGameLevel(event) {
+  $('.nav li.active').removeClass('active');
+  $(event.currentTarget).addClass('active');
+  var selectedLevel = $(event.currentTarget).data('level');
+
+  $('#main-container').removeClass('low').removeClass('medium').removeClass('high').addClass(selectedLevel);
+  Mz.selectedLevel = Mz.LEVELS[selectedLevel.toUpperCase()];
+  console.log(Mz.selectedLevel);
+  refreshObjects();
+}
+
+function refreshObjects() {
+  video = document.getElementById('video');
+  video.mozSrcObject = null;
+  
+  Mz.snapshotTimestamps = [];
+  Mz.numSnapshots = 0;
 }
